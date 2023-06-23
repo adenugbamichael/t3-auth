bba8702f7b23888ba7d14d9671a5144ce6835961
adenugbamichael <adenugbamicke@gmail.com>
2023-06-23 08:49:17 +0100

    login

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -8,7 +8,7 @@ import {
 import DiscordProvider from "next-auth/providers/discord";
 import { env } from "	/env.mjs";
 import { prisma } from "	/server/db";
-
+import EmailProvider from "next-auth/providers/email";
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
  * object and keep type safety.
@@ -47,6 +47,24 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER || "https://localhost:3000",
+        port: 587,
+        auth: {
+          user: "apikey",
+          pass: process.env.EMAIL_PASSWORD || "",
+        },
+      },
+      from: process.env.EMAIL_FROM || "default@default.com",
+      ...(process.env.NODE_ENV !== "production"
+        ? {
+            sendVerificationRequest({ url }) {
+              console.log("LOGIN LINK", url);
+            },
+          }
+        : {}),
+    }),
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
